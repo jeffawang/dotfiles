86bd4b5fce7843face67c9f4f05c2b6595f33e7e
Jeff Wang <jeffawang@gmail.com>
2024-11-08 18:22:39 -0800

    home.nix

--- home.nix
+++ home.nix
@@ -2,14 +2,27 @@
 
 let
   username = "jeffwang";
+  dotfiles = "$HOME/code/dotfiles";
 in {
   home = {
     inherit username;
     stateVersion = "24.05";
     homeDirectory = "/Users/${username}";
-    packages = [
 
+    # Attribute set for {ENV_VAR = ENV_VALUE}
+    #   sets it for all shells
+    # sessionVariables = {};
+
+    # Put packages here if they don't have configuration
+    packages = [
     ];
     file.stuff = {text = "things";};
+  };
+
+  # Configure program-specific stuff here
+  programs = {
+    bash = {
+      enable = true;
+    };
   };
 }
