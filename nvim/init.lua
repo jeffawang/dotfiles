26e24f31fb7909f2b86150939b8b261623335b23
Jeff Wang <jeffawang@gmail.com>
2026-04-22 15:46:31 -0700

    rm comment

--- nvim/init.lua
+++ nvim/init.lua
@@ -65,13 +65,6 @@ vim.o.formatexpr = "v:lua.require('conform').formatexpr()"
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>cq', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-
 vim.keymap.set({ 'i', 'n' }, '<C-j>', '<C-w>w', { desc = 'Move focus to the next window' })
 vim.keymap.set({ 'i', 'n' }, '<C-k>', '<C-w>W', { desc = 'Move focus to the previous window' })
 
